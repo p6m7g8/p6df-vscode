6d07fbb5169d416fdbafd9df0b91b939d69efe30
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-07 15:32:21 -0400

    chore(doc): regens

---
+++ README.md
@@ -0,0 +1,7 @@
+### init.zsh:
+- p6df::modules::vscode::deps()
+- p6df::modules::vscode::external::brew()
+- p6df::modules::vscode::home::symlink()
+- p6df::modules::vscode::init()
+- p6df::modules::vscode::version()
+
--- init.zsh
+++ init.zsh
@@ -1,6 +1,27 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::vscode::version()
+#
+#>
+######################################################################
 p6df::modules::vscode::version() { echo "0.0.1" }
+######################################################################
+#<
+#
+# Function: p6df::modules::vscode::deps()
+#
+#>
+######################################################################
 p6df::modules::vscode::deps()    { ModuleDeps=() }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::vscode::external::brew()
+#
+#>
+######################################################################
 p6df::modules::vscode::external::brew() {
 
   brew cask install visual-studio
@@ -8,14 +29,27 @@ p6df::modules::vscode::external::brew() {
   brew cask install visual-studio-code-insiders
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::vscode::home::symlink()
+#
+#>
+######################################################################
 p6df::modules::vscode::home::symlink() {
 
   ## fill in template
   ln -fs $P6_DFZ_SRC_P6M7G8_DIR/p6df-vscode/share/settings.json $HOME/Library/Applications\ Support/Code/User/settings.json
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::vscode::init()
+#
+#>
+######################################################################
 p6df::modules::vscode::init() {
 
   true
 }
-
