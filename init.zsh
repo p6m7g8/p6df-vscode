32da16fe060ba71b5c66cc80268d8206f3c5297c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-19 12:44:31 -0400

    feat(deps): installs python deps
    Should these move to python?

--- init.zsh
+++ init.zsh
@@ -1,5 +1,12 @@
-p6df::modules::vscode::version() { echo "0.0.1" }
-p6df::modules::vscode::deps()    { ModuleDeps=() }
+# shellcheck shell=zsh
+p6df::modules::vscode::version() {
+  echo "0.0.1"
+}
+
+p6df::modules::vscode::deps() {
+
+  ModuleDeps=()
+}
 
 p6df::modules::vscode::external::brew() {
 
@@ -14,8 +21,30 @@ p6df::modules::vscode::home::symlink() {
   ln -fs $P6_DFZ_SRC_P6M7G8_DIR/p6df-vscode/share/settings.json $HOME/Library/Applications\ Support/Code/User/settings.json
 }
 
-p6df::modules::vscode::init() {
+p6df::modules::vscode::external::brew() {
 
-  true
+  brew cask install kite
 }
 
+p6df::modules::vscode::langs() {
+
+  pip install -q nose
+  pip install -q pylint
+  pip install -q prospector
+  pip install -q mypy
+  pip install -q pylama
+  pip install -q pydocstyle
+  pip install -q flake9
+  pip install -q bandit
+  pip install -q pycodestyle
+
+  pip install -q pep8
+  pip install --upgrade autopep8
+  pip install black
+  pip install yapf
+  pip install jedi
+}
+
+p6df::modules::vscode::init() {
+  true
+}
